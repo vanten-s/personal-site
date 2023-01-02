49582074552ea23cb18cabd79d86b0ea9be3225f
vanten-s <svante.spannar@gmail.com>
2023-01-02 14:39:14 +0100

    Added rickroll

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 import frmWrk.website as frmWrk
 
-webServer = frmWrk.WebServer( "127.0.0.1", 8080, "./assets/" )
+webServer = frmWrk.WebServer( "127.0.0.1", 8080, "./assets" )
 webServer.start( )
 
 
